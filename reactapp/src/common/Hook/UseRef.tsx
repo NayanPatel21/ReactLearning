f18a6d30afb5b30e45bdca62688c4c6a4fb17d85
NayanPatel21 <110758301+NayanPatel21@users.noreply.github.com>
2025-08-10 15:46:46 +1000

    Updated UseRef

--- reactapp/src/common/Hook/UseRef.tsx
+++ reactapp/src/common/Hook/UseRef.tsx
@@ -4,6 +4,10 @@ const UseRef = () => {
   const [number, setNumber] = useState(0);
   const numberref = useRef(0);
 
+  const inputRef1 = useRef<HTMLInputElement | null>(null);
+  const inputRef2 = useRef<HTMLInputElement | null>(null);
+  const inputRef3 = useRef<HTMLInputElement | null>(null);
+
   useEffect(() => {
     console.log("Clicked and render");
   });
@@ -16,6 +20,27 @@ const UseRef = () => {
     numberref.current++;
   };
 
+  const btnRefInputclick1 = () => {
+    inputRef1.current.style.backgroundColor = "yellow";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+    inputRef1.current?.focus();
+  };
+
+  const btnRefInputclick2 = () => {
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "yellow";
+    inputRef3.current.style.backgroundColor = "";
+    inputRef2.current?.focus();
+  };
+
+  const btnRefInputclick3 = () => {
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "yellow";
+    inputRef3.current?.focus();
+  };
+
   return (
     <div>
       <div className="box">
@@ -26,6 +51,22 @@ const UseRef = () => {
       <div className="box">
         <h1> Use Ref {numberref.current}</h1>
         <button onClick={btnRefClick}>Click Me Ref</button>
+      </div>{" "}
+      <br></br>
+      <div className="box">
+        <h1> Use Ref Input</h1>
+        <div className="box">
+          <input ref={inputRef1} type="text"></input>&nbsp;&nbsp;
+          <button onClick={btnRefInputclick1}>Click Me button Ref1</button>
+        </div>
+        <div className="box">
+          <input ref={inputRef2} type="text"></input>&nbsp;&nbsp;
+          <button onClick={btnRefInputclick2}>Click Me button Ref2</button>
+        </div>
+        <div className="box">
+          <input ref={inputRef3} type="text"></input>&nbsp;&nbsp;
+          <button onClick={btnRefInputclick3}>Click Me button Ref3</button>
+        </div>
       </div>
     </div>
   );
